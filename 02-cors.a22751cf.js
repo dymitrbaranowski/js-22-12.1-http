f3bb2aea957687eef4aff781366e5159db60ddc7
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-02 18:51:22 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-22-12.1-http@f863cff5363494be5c7c16d4bc41bf4ade4ff69c 🚀

---
+++ 02-cors.a22751cf.js
@@ -0,0 +1,2 @@
+fetch("https://lpj-weather-service.herokuapp.com/weather?lat=39.916668&lon=116.383331").then((function(e){return e.json()})).then(console.log);
+//# sourceMappingURL=02-cors.a22751cf.js.map
