23fdd0e2bde3a5cf47c9c79c553a2ff761ad6936
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-05 14:06:41 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-22-12.1-http@4a75e86b6421f8c83f7c0ee6d22c1204377a438b 🚀

--- 03-pagination.c4fe540e.js
+++
@@ -1,2 +0,0 @@
-fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1",{headers:{Authorization:"fd4551f039144e50830dcd0945213250"}}).then((function(e){return e.json()})).then(console.log);
-//# sourceMappingURL=03-pagination.c4fe540e.js.map
--- 03-pagination.f0ae9afe.js
+++
@@ -1,2 +0,0 @@
-fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1",{headers:{Authorization:"fd4551f039144e50830dcd0945213250"}}).then((e=>e.json())).then(console.log);
-//# sourceMappingURL=03-pagination.f0ae9afe.js.map
